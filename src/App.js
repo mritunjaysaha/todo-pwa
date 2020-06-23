0f2b17fef704d502130166132d9b82a000247015
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-06-23 15:58:50 +0530

    added material ui tabs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AddTodo from "./components/add-todo.component";
+import CenteredTabs from "./components/tabs.component";
 function App() {
     return (
         <>
             <h3>Todo</h3>
-            <p>Active | Completed | Missed</p>
+            {/* <p>Active | Completed | Missed</p> */}
+            <CenteredTabs />
             <AddTodo />
             <hr />
         </>
--- src/components/todo.component.js
+++ src/components/todo.component.js
@@ -1,13 +1,7 @@
-import React, { useState, Component } from "react";
+import React from "react";
 import AddTodo from "./add-todo.component";
 
 export default function Todo() {
-    const [date, setDate] = useState({ date: Date.now() });
-
-    function onChange(date) {
-        setDate({ date });
-    }
-
     return (
         <>
             <h1>Todo</h1>
@@ -17,35 +11,3 @@ export default function Todo() {
         </>
     );
 }
-
-// export default class Todo extends Component {
-//     state = {
-//         date: new Date(),
-//         time: "10:00",
-//     };
-
-//     onChange = (date) => {
-//         console.log("date: ", date);
-//         this.setState({ date });
-//     };
-
-//     onChangeTime = (time) => {
-//         console.log("time: ", time);
-//         this.setState({ time });
-//     };
-
-//     render() {
-//         return (
-//             <div>
-//                 <DateTimePicker
-//                     onChange={this.onChange}
-//                     value={this.state.date}
-//                 />
-//                 <TimePicker
-//                     onChange={this.onChangeTime}
-//                     value={this.state.time}
-//                 />
-//             </div>
-//         );
-//     }
-// }
