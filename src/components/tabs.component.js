c79d40a4c7cdee2a72ffe2e68c40a20b4ece1e9e
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-06-23 16:06:39 +0530

    added react-router-dom

--- src/components/tabs.component.js
+++ src/components/tabs.component.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import { Link, Route, BrowserRouter, Switch } from "react-router-dom";
 
 const useStyles = makeStyles({
     root: {
@@ -17,20 +18,41 @@ export default function CenteredTabs() {
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
+    function ItemOne(theme) {
+        return (
+            <Paper>
+                <div>Item 1</div>
+            </Paper>
+        );
+    }
 
+    function ItemTwo(theme) {
+        return (
+            <Paper>
+                <div>Item two</div>
+            </Paper>
+        );
+    }
     return (
-        <Paper className={classes.root}>
-            <Tabs
-                value={value}
-                onChange={handleChange}
-                indicatorColor="primary"
-                textColor="primary"
-                centered
-            >
-                <Tab label="Item One" />
-                <Tab label="Item Two" />
-                <Tab label="Item Three" />
-            </Tabs>
-        </Paper>
+        <BrowserRouter>
+            <Paper className={classes.root}>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    centered
+                >
+                    <Tab label="Item One" component={Link} to="/one" />
+                    <Tab label="Item Two" component={Link} to="/two" />
+                    <Tab label="Item Three" />
+                </Tabs>
+            </Paper>
+
+            <Switch>
+                <Route path="/one" component={ItemOne} />
+                <Route path="/two" component={ItemTwo} />
+            </Switch>
+        </BrowserRouter>
     );
 }
